0e4f4c2b75bf696435883eda84648530d7a85de4
Adarsh Vyas <adarshvyas99@gmail.com>
2025-02-25 07:38:45 +0530

    setup task-1 branch

--- flask-api/app.py
+++ flask-api/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/run-ansible', methods=['GET'])
 def run_ansible():
-    result = subprocess.run(["ansible-playbook", "-i", "inventories/hosts", "playbooks/site.yml"], capture_output=True, text=True)
+    result = subprocess.run(["ansible-playbook", "-i", "inventory/hosts.yml", "playbooks/site.yml"], capture_output=True, text=True)
     return jsonify({"status": "success", "output": result.stdout})
 
 if __name__ == '__main__':
